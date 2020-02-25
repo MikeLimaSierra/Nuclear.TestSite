9dd5174af1821272c8a56135873ace0174fad1b0
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-02-25 21:13:51 +0100

    update test attributes

--- src/Nuclear.TestSite.Contracts/TestClassAttribute.cs
+++ src/Nuclear.TestSite.Contracts/TestClassAttribute.cs
@@ -15,16 +15,45 @@ namespace Nuclear.TestSite {
         /// </summary>
         public TestMode TestMode { get; private set; }
 
+        /// <summary>
+        /// Gets the reason why the corresponding test class is being ignored.
+        /// </summary>
+        public String IgnoreReason { get; private set; }
+
+        /// <summary>
+        /// Gets if the corresponding test class is being ignored.
+        /// </summary>
+        public Boolean IsIgnored => !String.IsNullOrWhiteSpace(IgnoreReason);
+
         #endregion
 
         #region ctors
 
         /// <summary>
+        /// Creates a new instance of <see cref="TestClassAttribute"/> with <see cref="TestMode.Parallel"/>.
+        /// </summary>
+        public TestClassAttribute() : this(TestMode.Parallel, null) { }
+
+        /// <summary>
         /// Creates a new instance of <see cref="TestClassAttribute"/>.
         /// </summary>
-        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding class. Default is <see cref="TestMode.Parallel"/>.</param>
-        public TestClassAttribute(TestMode testMode = TestMode.Parallel) {
-            TestMode = testMode;
+        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding test class.</param>
+        public TestClassAttribute(TestMode testMode) : this(testMode, null) { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestClassAttribute"/> with <see cref="TestMode.Parallel"/>.
+        /// </summary>
+        /// <param name="ignoreReason">The reason why this test class is being ignored.</param>
+        public TestClassAttribute(String ignoreReason) : this(TestMode.Parallel, ignoreReason) { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestClassAttribute"/>.
+        /// </summary>
+        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding test method.</param>
+        /// <param name="ignoreReason">The reason why this test class is being ignored.</param>
+        public TestClassAttribute(TestMode testMode, String ignoreReason) {
+            TestMode = Enum.IsDefined(typeof(TestMode), testMode) ? testMode : TestMode.Parallel;
+            IgnoreReason = ignoreReason;
         }
 
         #endregion
--- src/Nuclear.TestSite.Contracts/TestMethodAttribute.cs
+++ src/Nuclear.TestSite.Contracts/TestMethodAttribute.cs
@@ -15,16 +15,45 @@ namespace Nuclear.TestSite {
         /// </summary>
         public TestMode TestMode { get; private set; }
 
+        /// <summary>
+        /// Gets the reason why the corresponding test method is being ignored.
+        /// </summary>
+        public String IgnoreReason { get; private set; }
+
+        /// <summary>
+        /// Gets if the corresponding test method is being ignored.
+        /// </summary>
+        public Boolean IsIgnored => !String.IsNullOrWhiteSpace(IgnoreReason);
+
         #endregion
 
         #region ctors
 
         /// <summary>
+        /// Creates a new instance of <see cref="TestMethodAttribute"/> with <see cref="TestMode.Parallel"/>.
+        /// </summary>
+        public TestMethodAttribute() : this(TestMode.Parallel, null) { }
+
+        /// <summary>
         /// Creates a new instance of <see cref="TestMethodAttribute"/>.
         /// </summary>
-        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding test method. Default is <see cref="TestMode.Parallel"/>.</param>
-        public TestMethodAttribute(TestMode testMode = TestMode.Parallel) {
-            TestMode = testMode;
+        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding test method.</param>
+        public TestMethodAttribute(TestMode testMode) : this(testMode, null) { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestMethodAttribute"/> with <see cref="TestMode.Parallel"/>.
+        /// </summary>
+        /// <param name="ignoreReason">The reason why this test method is being ignored.</param>
+        public TestMethodAttribute(String ignoreReason) : this(TestMode.Parallel, ignoreReason) { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="TestMethodAttribute"/>.
+        /// </summary>
+        /// <param name="testMode">The <see cref="TestMode"/> of the corresponding test method.</param>
+        /// <param name="ignoreReason">The reason why this test method is being ignored.</param>
+        public TestMethodAttribute(TestMode testMode, String ignoreReason) {
+            TestMode = Enum.IsDefined(typeof(TestMode), testMode) ? testMode : TestMode.Parallel;
+            IgnoreReason = ignoreReason;
         }
 
         #endregion
