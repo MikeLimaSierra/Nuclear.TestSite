6495653dd206a780ca92dcbb513be7f838b27ca6
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2020-04-28 12:12:13 +0200

    Create ThrowingIEqualityComparer.cs

---
+++ src/Nuclear.TestSite.Dummies/TestComparers/ThrowingIEqualityComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace Nuclear.TestSite.TestComparers {
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public class ThrowingIEqualityComparer : IEqualityComparer {
+
+        public new Boolean Equals(Object x, Object y) => throw new NotImplementedException();
+
+        public Int32 GetHashCode(Object obj) => throw new NotImplementedException();
+
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+}
